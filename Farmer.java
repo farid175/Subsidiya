82991a77aa9864629e15b09b240ae04dd966c203
farid175 <50070768+farid175@users.noreply.github.com>
2019-09-03 19:28:37 +0400

    Create Farmer.java

---
+++ Farmer.java
@@ -0,0 +1,188 @@
+ public class Farmer {
+        private String sira;
+        private String sname;
+        private String cemi_ha;
+        private String arpa;
+        private String qargidali;
+        private String gun_bax;
+        private String pambiq;
+        private String sek_cug;
+        private String kartof;
+        private String terevez;
+        private String bostan;
+        private String uzum;
+        private String meyve;
+        private String cay;
+        private String diger;
+        private String kend;
+        private String adrayon;
+        private String rayon;
+
+        @Override
+        public String toString() {
+            return "Farmer{" +
+                    "sira='" + sira + '\'' +
+                    ", sname='" + sname + '\'' +
+                    ", cemi_ha='" + cemi_ha + '\'' +
+                    ", arpa='" + arpa + '\'' +
+                    ", qargidali='" + qargidali + '\'' +
+                    ", gun_bax='" + gun_bax + '\'' +
+                    ", pambiq='" + pambiq + '\'' +
+                    ", sek_cug='" + sek_cug + '\'' +
+                    ", kartof='" + kartof + '\'' +
+                    ", terevez='" + terevez + '\'' +
+                    ", bostan='" + bostan + '\'' +
+                    ", uzum='" + uzum + '\'' +
+                    ", meyve='" + meyve + '\'' +
+                    ", cay='" + cay + '\'' +
+                    ", diger='" + diger + '\'' +
+                    ", kend='" + kend + '\'' +
+                    ", adrayon='" + adrayon + '\'' +
+                    ", rayon='" + rayon + '\'' +
+                    '}';
+        }
+
+        public String getSira() {
+            return sira;
+        }
+
+        public void setSira(String sira) {
+            this.sira = sira;
+        }
+
+        public String getSname() {
+            return sname;
+        }
+
+        public void setSname(String sname) {
+            this.sname = sname;
+        }
+
+        public String getCemi_ha() {
+            return cemi_ha;
+        }
+
+        public void setCemi_ha(String cemi_ha) {
+            this.cemi_ha = cemi_ha;
+        }
+
+        public String getArpa() {
+            return arpa;
+        }
+
+        public void setArpa(String arpa) {
+            this.arpa = arpa;
+        }
+
+        public String getQargidali() {
+            return qargidali;
+        }
+
+        public void setQargidali(String qargidali) {
+            this.qargidali = qargidali;
+        }
+
+        public String getGun_bax() {
+            return gun_bax;
+        }
+
+        public void setGun_bax(String gun_bax) {
+            this.gun_bax = gun_bax;
+        }
+
+        public String getPambiq() {
+            return pambiq;
+        }
+
+        public void setPambiq(String pambiq) {
+            this.pambiq = pambiq;
+        }
+
+        public String getSek_cug() {
+            return sek_cug;
+        }
+
+        public void setSek_cug(String sek_cug) {
+            this.sek_cug = sek_cug;
+        }
+
+        public String getKartof() {
+            return kartof;
+        }
+
+        public void setKartof(String kartof) {
+            this.kartof = kartof;
+        }
+
+        public String getTerevez() {
+            return terevez;
+        }
+
+        public void setTerevez(String terevez) {
+            this.terevez = terevez;
+        }
+
+        public String getBostan() {
+            return bostan;
+        }
+
+        public void setBostan(String bostan) {
+            this.bostan = bostan;
+        }
+
+        public String getUzum() {
+            return uzum;
+        }
+
+        public void setUzum(String uzum) {
+            this.uzum = uzum;
+        }
+
+        public String getMeyve() {
+            return meyve;
+        }
+
+        public void setMeyve(String meyve) {
+            this.meyve = meyve;
+        }
+
+        public String getCay() {
+            return cay;
+        }
+
+        public void setCay(String cay) {
+            this.cay = cay;
+        }
+
+        public String getDiger() {
+            return diger;
+        }
+
+        public void setDiger(String diger) {
+            this.diger = diger;
+        }
+
+        public String getKend() {
+            return kend;
+        }
+
+        public void setKend(String kend) {
+            this.kend = kend;
+        }
+
+        public String getAdrayon() {
+            return adrayon;
+        }
+
+        public void setAdrayon(String adrayon) {
+            this.adrayon = adrayon;
+        }
+
+        public String getRayon() {
+            return rayon;
+        }
+
+        public void setRayon(String rayon) {
+            this.rayon = rayon;
+        }
+    }
